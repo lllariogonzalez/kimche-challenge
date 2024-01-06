956db64cd2eac82f6379256d291807b134fc56f6
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-06 20:03:35 -0300

    feat: add use character hook, loader, header and characters component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Characters from './components/Characters'
+import Loader from './components/Loader'
+import Header from './components/Header'
+import { useCharacters } from './characters/custom-hooks'
+import Error from './components/Error'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const { data, error, loading } = useCharacters()
+
+  if (error) {
+    return (
+      <Error>
+        {error.message}
+      </Error>
+    )
+  }
 
   return (
-    <>
-      <div className='flex justify-center'>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Kimche challenge</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <main className='flex flex-col items-center justify-center min-h-screen max-w-7xl m-auto p-8'>
+      {loading
+        ? <Loader />
+        : (
+          <>
+            <Header />
+            <Characters characters={data.characters.results} />
+          </>
+          )}
+    </main>
   )
 }
 
