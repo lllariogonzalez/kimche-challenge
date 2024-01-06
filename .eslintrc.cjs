385e04de21164d8f9cf80ce81e13091092cb97c5
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-06 12:39:56 -0300

    build: initial app react + vite, configured tailwind css, eslint, and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: { 
+    react: { version: '18.2' },
+    'import/resolver': {
+      alias: {
+        map: [
+          [['@', './src']], // Configura alias '@' para que apunte a './src'
+        ],
+        extensions: ['.js', '.jsx', '.json']
+      }
+    }
+  },
+  plugins: [
+    'react',
+    'import',
+    'react-refresh'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'import/no-absolute-path': 'off',
+  }
+}
