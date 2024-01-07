d1597dbdf3c1f46cd9d5e3969092226ebacb6981
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-07 19:31:05 -0300

    feat: add hardcord filters options species, status and gender

---
+++ src/characters/filters-options.js
@@ -0,0 +1,24 @@
+export const speciesList = [
+  'Human',
+  'Alien',
+  'Robot',
+  'Cronenberg',
+  'Animal',
+  'Mythological Creature',
+  'Humanoid',
+  'Disease',
+  'Unknown'
+]
+
+export const statusList = [
+  'Alive',
+  'Dead',
+  'Unknown'
+]
+
+export const genderList = [
+  'Male',
+  'Female',
+  'Genderless',
+  'Unknown'
+]
