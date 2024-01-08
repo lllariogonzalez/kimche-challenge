285d84526a227aea1972ac26dc7bf7cf92e8452e
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-08 11:37:00 -0300

    feat: add hook use character for use lazy query and get character datails by id

--- src/characters/custom-hooks.js
+++ src/characters/custom-hooks.js
@@ -1,5 +1,5 @@
 import { useLazyQuery } from '@apollo/client'
-import { GET_CHARACTERS } from './graphql-queries'
+import { GET_CHARACTER, GET_CHARACTERS } from './graphql-queries'
 import { useCallback, useContext, useEffect } from 'react'
 import { FiltersContext } from '../context/FiltersContext'
 import debounce from 'just-debounce-it'
@@ -22,4 +22,10 @@ export function useCharacters () {
   }, [debounceGetCharacters, filters])
 
   return { characters, error, loading }
+}
+
+export function useCharacter () {
+  const [getCharacter, { data, error, loading }] = useLazyQuery(GET_CHARACTER)
+  const character = data?.character
+  return [getCharacter, { character, error, loading }]
 }
