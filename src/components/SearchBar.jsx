43de616357eef2b0a36a148c6e8791fe95d8138f
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-07 21:34:00 -0300

    refactor: add use context to search bar component and set filters on change, delete prop drilling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import PropTypes from 'prop-types'
-import SearchIcon from '../assets/SearchIcon'
+import { useContext } from 'react'
+import SearchIcon from '@/assets/SearchIcon'
+import { FiltersContext } from '@/context/FiltersContext'
 
-export default function SearchBar ({ onSearch }) {
-  const [search, setSearch] = useState('')
-
-  const handleInputChange = (event) => {
-    setSearch(event.target.value)
-  }
+export default function SearchBar () {
+  const { filters, setFilters } = useContext(FiltersContext)
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    onSearch(search)
+    setFilters({ ...filters, name: filters.name })
+  }
+
+  const handleInputChange = (event) => {
+    const name = event.target.value
+    setFilters({ ...filters, name })
   }
 
   return (
     <form onSubmit={handleSubmit} className='flex w-full h-12 items-center border-2 border-gray-300 rounded-xl overflow-hidden'>
       <input
         type='text'
-        value={search}
+        value={filters.name}
         onChange={handleInputChange}
         placeholder='Rick Sanchez'
         className='px-2 py-1 h-full bg-inherit focus:outline-none focus:border-none flex-grow'
       />
-      <button type='submit' className='h-full bg-white text-black px-3 py-1 flex items-center justify-center'>
+      <button
+        type='submit'
+        className='h-full bg-white text-black px-3 py-1 flex items-center justify-center'
+        aria-label='Search characters'
+      >
         <SearchIcon className='w-4 h-4' />
       </button>
     </form>
   )
-}
-
-SearchBar.propTypes = {
-  onSearch: PropTypes.func.isRequired
 }
