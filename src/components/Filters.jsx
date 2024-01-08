d13857efe2ed9c7a849c39a5faed9845d30974f6
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-07 21:25:56 -0300

    feat: add filters component with select by status, species, gender and reset filters button

---
+++ src/components/Filters.jsx
@@ -0,0 +1,63 @@
+import Select from './Select'
+import { useContext } from 'react'
+import { FiltersContext } from '@/context/FiltersContext'
+import { genderList, speciesList, statusList } from '@/characters/filters-options'
+
+export default function Filters () {
+  const { filters, setFilters } = useContext(FiltersContext)
+
+  const handleSelectFilters = (event) => {
+    const { name, value } = event.target
+    setFilters({ ...filters, [name]: value })
+  }
+
+  const handleReset = () => {
+    setFilters({
+      name: '',
+      status: '',
+      species: '',
+      gender: ''
+    })
+  }
+
+  return (
+    <div className='flex flex-col md:flex-row w-full text-white gap-4'>
+
+      <Select value={filters.status} name='status' onChange={handleSelectFilters}>
+        <option value=''>Status...</option>
+        {statusList.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </Select>
+
+      <Select value={filters.species} name='species' onChange={handleSelectFilters}>
+        <option value=''>Species...</option>
+        {speciesList.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </Select>
+
+      <Select value={filters.gender} name='gender' onChange={handleSelectFilters}>
+        <option value=''>Genders...</option>
+        {genderList.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </Select>
+
+      <button
+        onClick={handleReset}
+        className='font-bold w-full md:w-fit text-nowrap bg-white rounded-lg px-4 py-1 text-black'
+        aria-label='Reset filters'
+      >
+        Reset filters
+      </button>
+
+    </div>
+  )
+}
