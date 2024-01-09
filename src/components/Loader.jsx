e2e28ce1db6e1c0e2f2a3fa1e279fb3e8edfbbe3
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-09 10:33:11 -0300

    fix: fix loading error in vercel, move portal.gif to public folder

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 export default function Loader () {
   return (
-    <img className='w-40 h-fit' src='src/assets/portal.gif' alt='Portal Rick and Morty' />
+    <img className='w-40 h-fit' src='portal.gif' alt='Portal Rick and Morty' />
   )
 }
