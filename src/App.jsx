73a54a597f87e8d4c1fce783fcaef96a0e438412
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-10 22:28:23 -0300

    style: add bg radial gradient black to green

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import { FiltersProvider } from '@/context/FiltersContext'
 
 function App () {
   return (
-    <main className='flex flex-col items-center justify-start min-h-screen max-w-7xl my-10 mx-5 lg:mx-auto gap-8'>
-      <Header />
-      <FiltersProvider>
-        <SearchBar />
-        <Filters />
-        <Characters />
-      </FiltersProvider>
-    </main>
+    <div className='absolute top-0 w-full max-w-screen [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#00ff00_100%)]'>
+      <main className='flex flex-col items-center justify-start min-h-screen max-w-7xl my-10 mx-5 lg:mx-auto gap-8 '>
+        <Header />
+        <FiltersProvider>
+          <SearchBar />
+          <Filters />
+          <Characters />
+        </FiltersProvider>
+      </main>
+    </div>
   )
 }
 
