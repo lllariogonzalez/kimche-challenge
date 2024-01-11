b9668d9504ee0a4a7904b567d188de7887ed5884
lllariogonzalez <lllariogonzalez@gmail.com>
2024-01-11 08:36:17 -0300

    feat: add prev left icon

---
+++ src/components/Icons/PrevIcon.jsx
@@ -0,0 +1,7 @@
+export default function PrevIcon () {
+  return (
+    <svg fill='none' viewBox='0 0 24 24' strokeWidth='2.5' stroke='currentColor' className='w-6 h-6'>
+      <path strokeLinecap='round' strokeLinejoin='round' d='m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5' />
+    </svg>
+  )
+}
